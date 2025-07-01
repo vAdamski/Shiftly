ee7d23b00d449ba69191d47e04d49c5946050e8d
Adam Ludwiczak <adam.ludwiczak98@gmail.com>
2025-07-01 18:08:10 +0200

    Add initial project structure with BaseApiController and Domain class

---
+++ Shiftly.Api/Controllers/BaseApiController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Shiftly.Api.Controllers;
+
+[ApiController]
+public class BaseApiController  : ControllerBase
+{
+    
+}
---
+++ Shiftly.Domain/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace Shiftly.Domain;
+
+public class Class1
+{
+}
